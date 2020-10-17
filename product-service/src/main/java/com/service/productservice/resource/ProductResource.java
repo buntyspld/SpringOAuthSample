7ed39c4dd12f2eb809e6d287640c8291e65789ee
Saurabh Naik <buntyspld@gmail.com>
2020-10-17 22:51:07 +0530

    All products

---
+++ product-service/src/main/java/com/service/productservice/resource/ProductResource.java
@@ -0,0 +1,21 @@
+package com.service.productservice.resource;
+
+import com.service.productservice.model.Product;
+import com.service.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class ProductResource {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> allProducts(){
+        return productService.allProducts();
+    }
+}
---
+++ product-service/src/main/java/com/service/productservice/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.service.productservice.service;
+
+import com.service.productservice.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class ProductService {
+    private static List<Product> allProduct = new ArrayList<>();
+
+    public ProductService() {
+        allProduct.add(new Product(1, "Nokia 3310", "Mobile", 8000));
+        allProduct.add(new Product(2, "Nokia 6600", "Mobile", 18000));
+        allProduct.add(new Product(3, "Playstation 4", "Gaming", 38000));
+        allProduct.add(new Product(4, "Super Mario", "Video Game", 3000));
+    }
+
+    public List<Product> allProducts() {
+        return allProduct;
+    }
+
+
+}

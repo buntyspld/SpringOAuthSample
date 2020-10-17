8fa785f04d4794bba1e4396a5a4fca538ee94a59
Saurabh Naik <buntyspld@gmail.com>
2020-10-18 04:13:08 +0530

    Discovery Server

--- food-discovery-server/food-discovery-server/src/main/java/com/services/fooddiscoveryserver/FoodDiscoveryServerApplication.java
+++ food-discovery-server/food-discovery-server/src/main/java/com/services/fooddiscoveryserver/FoodDiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.services.fooddiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class FoodDiscoveryServerApplication {
 

1d91f33de91354dbbd1c7a93d93557d5a75ea63e
Swapna <Swapna@LAPTOP-B2L6EL3A>
2020-10-24 13:21:28 +0530

    registered Orders services with eureka

--- orders-service/src/main/java/com/service/ordersservice/OrdersServiceApplication.java
+++ orders-service/src/main/java/com/service/ordersservice/OrdersServiceApplication.java
@@ -2,8 +2,10 @@ package com.service.ordersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrdersServiceApplication {
 
 	public static void main(String[] args) {
